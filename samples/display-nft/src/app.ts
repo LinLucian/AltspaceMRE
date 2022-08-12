948d98b7ea5d03d63b2e54dd6ab58dd7b955873e
Lucian Lin <lin.lucian@gmail.com>
2022-08-12 10:49:31 -0400

    final update!

--- samples/display-nft/src/app.ts
+++ samples/display-nft/src/app.ts
@@ -2,37 +2,145 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-
+import Web3 from "web3";
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import { AbiItem } from "web3-utils";
 
-const _importDynamic = new Function('modulePath', 'return import(modulePath)')
+const web3 = new Web3('https://mainnet.infura.io/v3/6b4a6b9998e94db182a48a6ccf3dee55');
+const _importDynamic = new Function('modulePath', 'return import(modulePath)');
+
 async function fetch(...args: string[]) {
 	const { default: fetch } = await _importDynamic('node-fetch');
 	return fetch(...args);
+
 }
+
 /**
  * The main class of this app. All the logic goes here.
  */
 export default class HelloWorld {
+	readonly abi: AbiItem[] = [
+		{
+			"constant": true,
+			"inputs": [
+				{
+					"name": "_tokenId",
+					"type": "uint256"
+				}
+			],
+			"name": "tokenURI",
+			"outputs": [
+				{
+					"name": "",
+					"type": "string"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function",
+		},
+	];
 
 	private text: MRE.Actor = null;
+	private display: MRE.Actor = null;
 	private cube: MRE.Actor = null;
 	private assets: MRE.AssetContainer;
 	private response: string;
+	private inputContactAddress: string;
+	private inputTokenID: string;
 
-	constructor(private context: MRE.Context) {
+	constructor(private context: MRE.Context, private params: MRE.ParameterSet) {
 		this.context.onStarted(() => this.started());
+		console.log('Params:', params);
+	}
+
+	private async getNFTDetails(contractAddress: string, tokenId: string): Promise<string> {
+		const contract = new web3.eth.Contract(this.abi, String(contractAddress));
+		const tokenURI = await contract.methods.tokenURI(String(tokenId)).call();
+		const filterTokenURI = tokenURI.replace(/^ipfs?:\/\//, '');
+		const TokenURIlink = "https://ipfs.io/ipfs/" + filterTokenURI;
+		console.log(TokenURIlink);
+		const response = await fetch(TokenURIlink);
+		console.log(response);
+		const data = await response.json();
+		const filterData = data["image"].replace(/^ipfs?:\/\//, '');
+		const final = "https://ipfs.io/ipfs/" + filterData;
+		return final;
 	}
 
 	/**
 	 * Once the context is "started", initialize the app.
 	 */
-	private async started() {
+	private started() {
 		// set up somewhere to store loaded assets (meshes, textures, animations, gltfs, etc.)
 		this.assets = new MRE.AssetContainer(this.context);
-		await fetch("http://10.180.36.185:4002/?contractAddress=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D&tokenId=5673").then(async res => { this.response = await res.text(); })
-		console.log(this.response);
-		
+
+		// create button for NFT contact address input
+		const inputObject1 = MRE.Actor.Create(this.context, {
+			actor: {
+				name: "inputObject",
+				appearance: {
+					meshId: this.assets.createBoxMesh('box', 0.1, 0.1, 0.1).id
+				},
+				transform: {
+					local: {
+						position: { x: -1, y: 0, z: 0 }
+					}
+				},
+				collider: {
+					layer: MRE.CollisionLayer.Hologram,
+					geometry: {
+						shape: MRE.ColliderType.Auto
+					}
+				}
+			}
+		});
+
+		// set the NFT contact address input button to prompt user response
+		const inputBehavior1 = inputObject1.setBehavior(MRE.ButtonBehavior);
+		inputBehavior1.onClick(click => {
+			click.prompt("Enter the NFT contact address:", true)
+				.then(result => {
+					if (result.submitted) {
+						this.inputContactAddress = result.text;
+						console.log(result.text);
+						if (this.display !== null){
+							this.display.destroy();
+							this.display = null;
+						}
+						if (this.text !== null){
+							this.text.destroy();
+							this.text = null;
+						}
+					}
+				})
+				.catch(err => {
+					console.error(err)
+				});
+			click.prompt("Enter the NFT token ID:", true)
+				.then(result => {
+					if (result.submitted) {
+						this.inputTokenID = result.text;
+						console.log(result.text);
+						this.generateNFT();
+					}
+				})
+				.catch(err => {
+					console.error(err)
+				});
+		});
+
+		// const inputA = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
+		// const inputB = "3571";
+
+		//call getNFTDetails function and apply the image url to imageTexture
+
+	}
+	private async generateNFT() {
+
+		this.response = await this.getNFTDetails(this.inputContactAddress, this.inputTokenID);
+		console.log('Response:', this.response);
+a
 		const imageTexture = this.assets.createTexture("picture", {
 			uri: this.response
 		});
@@ -44,7 +152,7 @@ export default class HelloWorld {
 			emissiveColor: new MRE.Color4(1, 1, 1, 1)
 		});
 
-		const display = MRE.Actor.Create(this.context, {
+		this.display = MRE.Actor.Create(this.context, {
 			actor: {
 				name: "displayImage",
 				appearance: {
@@ -73,7 +181,7 @@ export default class HelloWorld {
 					app: { position: { x: 0, y: 1, z: 0 } }
 				},
 				text: {
-					contents: "Your NFT",
+					contents: "Contact Address: " + this.inputContactAddress + " \n" + "Token ID: " + this.inputTokenID,
 					anchor: MRE.TextAnchorLocation.MiddleCenter,
 					color: { r: 255 / 255, g: 255 / 255, b: 255 / 255 },
 					height: 0.1
@@ -100,7 +208,7 @@ export default class HelloWorld {
 		// Once the animation data is created, we can create a real animation from it.
 		spinAnimData.bind(
 			// We assign our text actor to the actor placeholder "text"
-			{ displayImage: display },
+			{ displayImage: this.display },
 			// And set it to play immediately, and bounce back and forth from start to end
 			{ isPlaying: true, wrapMode: MRE.AnimationWrapMode.PingPong });
 	}
